218fcf00654df39a6d7fa8ed43afceab29f7405d
Александр Пресняков <flagist@yandex-team.ru>
2017-05-11 23:03:00 +0300

    Removed pipeline

--- nplus1/pipelines.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-class Nplus1Pipeline(object):
-    def process_item(self, item, spider):
-        return item
--- nplus1/settings.py
+++ nplus1/settings.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for nplus1 project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#
-
 BOT_NAME = 'nplus1'
 
 SPIDER_MODULES = ['nplus1.spiders']
@@ -15,9 +7,5 @@ NEWSPIDER_MODULE = 'nplus1.spiders'
 
 USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/5.0)'
 AUTOTHROTTLE_ENABLED = True
-
-ITEM_PIPELINES = {
-    'nplus1.pipelines.Nplus1Pipeline': 300
-}
 
 DOWNLOAD_DELAY = 2
