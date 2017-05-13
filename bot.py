993bdc68df996da9d2e9fcf53c97cd1e529bc305
Александр Пресняков <flagist@yandex-team.ru>
2017-05-13 19:06:48 +0300

    Added search by author

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
+# encoding: utf-8
 import logging
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ParseMode
+from article import Article, ArticleIndex
 
 
 class SearchBot(object):
@@ -11,12 +13,32 @@ class SearchBot(object):
         token = self.get_token()
         self.updater = Updater(token=token)
 
-        self.updater.dispatcher.add_handler(CommandHandler('start', self.start_handler))
+        self.updater.dispatcher.add_handler(CommandHandler('start', SearchBot.show_database_info))
+        self.updater.dispatcher.add_handler(CommandHandler('info', SearchBot.show_database_info))
+        self.updater.dispatcher.add_handler(CommandHandler('author', SearchBot.search_by_author, pass_args=True))
         self.updater.start_polling()
 
     @staticmethod
-    def start_handler(bot, update):
-        bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    def show_database_info(bot, update):
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=u'В базе {} статей'.format(Article.parsed_num()))
+
+
+    @staticmethod
+    def search_by_author(bot, update, args):
+        author = ' '.join(args)
+
+        articles = Article.select().where(Article.author == author).order_by(Article.date.desc())
+        if articles.count():
+            lines = [u'Найдено {} статей:'.format(articles.count())]
+            lines += [u'[{}]({}) {}'.format(article.title, article.url, article.date) for article in articles]
+            output = '\n\n'.join(lines)
+        else:
+            output = u'Статей автора "{}" не найдено'.format(author)
+
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=output,
+                         parse_mode=ParseMode.MARKDOWN)
 
     @staticmethod
     def get_token():
